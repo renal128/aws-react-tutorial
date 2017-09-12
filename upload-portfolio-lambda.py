d15667a40a5b7b4930efe35f10f2d43f3ec9d2ff
Renal <renal128@gmail.com>
2017-09-11 22:34:07 -0700

    Add build scrypt

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,18 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+portfolio_bucket = s3.Bucket('portfolio.thegymio.com')
+build_bucket = s3.Bucket('portfoliobuild.thegymio.com')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        content_type = {'ContentType': mimetypes.guess_type(nm)[0]}
+        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs=content_type)
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
